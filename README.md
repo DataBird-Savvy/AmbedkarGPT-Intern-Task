f292ac78453ae28eac5729a1e3d480e6370a79ad
jiya-at-915921793449 <jiyamary.joseph@klearcom.com>
2025-11-17 15:14:13 +0530

    optimized the work flow

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # AmbedkarGPT-Intern-Task
 A simple command-line RAG system using LangChain, ChromaDB, and Ollama Mistral to answer questions from Dr. Ambedkar’s speech.
+src/
+│── cli.py                # Manual testing
+│── main.py               # FastAPI production server
+│── retriever.py          # Hybrid RAG retrieval
+│── vectordb.py           # Chroma DB build/load
+│── llm.py                # Ollama/Mistral client
+│── rag_pipeline.py       # Complete RAG pipeline
+│── embedding.py
+│── document_processor.py
+│── logger.py
+│── exception.py
+│── config.py
--- src/retriever.py
+++ src/retriever.py
@@ -4,8 +4,7 @@ from logger import logger
 from exception import RAGException
 from langchain_community.retrievers import BM25Retriever
 from dotenv import load_dotenv
-import os
-
+from langchain_core.documents import Document
 load_dotenv()
 
 
@@ -29,19 +28,27 @@ class Retriever:
         logger.info("Retrieving relevant documents from vector database.")
         
         try:
-            top_docs = self.vectordb.similarity_search(query, k=self.vector_k)
-            logger.info(f"Retrieved {len(top_docs)} docs from vector search.")
+            vector_docs = self.vectordb.similarity_search(query, k=self.vector_k)
+            logger.info(f"Retrieved {len(vector_docs)} docs from vector search.")
             
             
             
-            bm25_retriever = BM25Retriever.from_documents(top_docs)
+            all_docs = self.vectordb._collection.get(include=["documents"])["documents"]
+            all_docs = [Document(page_content=text) for text in all_docs]
+            bm25_retriever = BM25Retriever.from_documents(all_docs)
             bm25_docs = bm25_retriever.invoke(query) 
             logger.info("BM25Retriever reranking applied based on query.")
+            
+            
+            # Combine and remove duplicates
+            combined_docs = {doc.page_content: doc for doc in vector_docs + bm25_docs}
+            combined_docs = list(combined_docs.values())
+
 
                 
             
             reranker = CohereRerank(model=config.COHERE_RERANK_MODEL)
-            strings = [d.page_content for d in bm25_docs]
+            strings = [d.page_content for d in combined_docs]
             response = reranker.rerank(
                 query=query,
                 documents=strings,
@@ -71,9 +78,7 @@ if __name__ == "__main__":
     retriever = Retriever(db)
     query = "What is Ambedkar's view on social justice?"
     results = retriever.retrieve(query)
-    for i, res in enumerate(results):
-        print(f"Result {i+1}:\n{res.page_content}\n")
-
+   
 
 
 
